dc55082ec7d3edba777caa72e17ef839c93f90a6
trent bullard <trent.bullard@dhsgroup.com>
2019-05-17 15:05:15 -0500

    add auth to es requests

--- src/apis/es.js
+++ src/apis/es.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `${process.env.REACT_APP_ES_ROOT_URI}`
+  baseURL: `${process.env.REACT_APP_ES_ROOT_URI}`,
+  auth: {
+    username: `${process.env.REACT_APP_ES_USERNAME}`,
+    password: `${process.env.REACT_APP_ES_PASSWORD}`
+  }
 });
