f2e0a991a185615b70ac02905974ec0c8e20b1b6
trent bullard <trent.bullard@dhsgroup.com>
2019-05-17 15:14:36 -0500

    add https to eq2 api request

--- src/apis/eq2.js
+++ src/apis/eq2.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `http://census.daybreakgames.com/s:${
+  baseURL: `https://census.daybreakgames.com/s:${
     process.env.REACT_APP_EQ2_API_SECRET
   }/get/eq2`
 });
